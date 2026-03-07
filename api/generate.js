b7be520da38905111c1b731a8197653eebe7d45c
giovannibordi12-debug <giovannibordi12@gmail.com>
2026-03-07 22:38:56 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,13 @@
 module.exports = async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method Not Allowed' })
     return
